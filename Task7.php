a3d6e5e2e0a585e2d056eaeec2e8dc84df47c46f
Andrey <staybox@gmail.com>
2018-02-27 00:13:35 +0300

    Седьмое задание

--- Task7.php
+++ Task7.php
@@ -10,7 +10,13 @@ for ($row=1; $row <= 10; $row++) {
     echo "<tr> \n";
     for ($col=1; $col <= 10; $col++) {
         $p = $col * $row;
-        echo "<td>$p</td> \n";
+        if ($p%2 == 0) {
+            echo "<td>($p)</td> \n";
+        }elseif ($p%2 == 1) {
+            echo "<td>[$p]</td> \n";
+        }else {
+            echo "<td>$p</td> \n";
+        }
     }
     echo "</tr>";
 }
