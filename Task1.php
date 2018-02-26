0fe864fc3ce94044ef2e5e790d1522a3d036409d
Andrey <staybox@gmail.com>
2018-02-26 21:43:48 +0300

    Первый коммит

---
+++ Task1.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 26.02.2018
+ * Time: 21:20
+ */
+
+// CONST
+define("NAME", "Андрей");
+define("AGE", "30");
+
+// OUTPUT ON SCREEN
+
+echo "Меня зовут: " . NAME . "\n";
+echo "Мне " . AGE . " лет" . "\n";
+echo "\"!|\/'\"\\";
+
+exit;
