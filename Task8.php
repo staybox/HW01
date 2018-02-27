d7513f21165f14a6775e334862ee1ebcd975edb1
Andrey <staybox@gmail.com>
2018-02-27 13:42:48 +0300

    Восьмое задание (исправления)

--- Task8.php
+++ Task8.php
@@ -6,10 +6,18 @@
  * Time: 0:18
  */
 
-$str = "Строка с пробелами";
+$str = "Три ключевых принципа ПО, которые вы должны понимать";
 echo $str . "\n";
 $array = explode(" ", $str);
-echo $array[1] . $array[2] . "|" . $array[0];
+print_r($array);
+
+$count = count($array)-1;
+
+while ($count >= 0) {
+
+    echo $array[$count];
+    $count--;
+}
 
 
 
