d06233e1dfb0ce2b42a4cf97f73d3bc8fa27d1c8
Andrey <staybox@gmail.com>
2018-02-27 11:38:10 +0300

    Второе задание (исправления)

--- Task2.php
+++ Task2.php
@@ -7,16 +7,16 @@
  */
 
 // VARIABLES
-$all_pictures = 80;
-$felt_tip_pictures = 23;
-$pencil_pictures = 40;
+$allPictures = 80;
+$feltTipPictures = 23;
+$pencilPictures = 40;
 
 // OUTPUT ON SCREEN
-echo "Дана задача: На школьной выставке $all_pictures рисунков. $felt_tip_pictures из них выполнены фломастерами, $pencil_pictures карандашами, а остальные — красками.\nСколько рисунков, выполненные красками, на школьной выставке?\n";
+echo "Дана задача: На школьной выставке $allPictures рисунков. $feltTipPictures из них выполнены фломастерами, $pencilPictures карандашами, а остальные — красками.\nСколько рисунков, выполненные красками, на школьной выставке?\n";
 
 // Для того чтобы найти число картинок, выполненных красками, необходимо сложить 23+40=63, далее от 80 отнимем 63 и получим число 17. 17 картинок выполнены красками.
 
-$sum = $felt_tip_pictures + $pencil_pictures;
-$paints_pictures = $all_pictures - $sum;
+$sum = $felTipPictures + $pencilPictures;
+$paintsPictures = $allPictures - $sum;
 
-echo "Ответ: $paints_pictures";
+echo "Ответ: $paintsPictures";
